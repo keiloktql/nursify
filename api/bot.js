2aa524e5ea39b9bef4f8bcf3c4e33c58b119f5cf
keilokimnida <tkl48leon@gmail.com>
2024-02-03 11:43:20 +0800

    [feat] test

--- api/bot.js
+++ api/bot.js
@@ -1,5 +1,5 @@
-const { Bot, webhookCallback } = require("grammy");
-require("dotenv/config").config();
+import { Bot, webhookCallback } from "grammy";
+import "dotenv/config";
 
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
