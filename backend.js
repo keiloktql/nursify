e73c3c12264ed42ca5e875d7c72c1677e4cc0164
Chai Pin Zheng <chaipinzheng@gmail.com>
2024-02-03 15:12:53 +0800

    Adding OpenAI package and adding node_modules into gitignore

--- backend.js
+++ backend.js
@@ -1,3 +1,9 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+});
+
 export const OCR = (photo) => {
   return "";
 };
