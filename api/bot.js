ffb1e8cbc600d9bbebccadb290c44f5c2198640e
keilokimnida <tkl48leon@gmail.com>
2024-02-03 12:58:15 +0800

    [feat] bot

--- api/bot.js
+++ api/bot.js
@@ -1,32 +1,55 @@
-import { Bot, webhookCallback } from "grammy";
-import { Commands, commands } from "@grammyjs/commands";
+import { Bot, webhookCallback, Keyboard } from "grammy";
+import { Menu } from "@grammyjs/menu";
 import "dotenv/config";
 
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
 
 const bot = new Bot(token);
-bot.use(commands());
 
 // MENU
-// const mainNenu = new Menu("main")
-//   .text("Explain medical condition", (ctx) =>
-//     ctx.reply("You pressed medical condition")
-//   )
-//   .row()
-//   .text("Explain medication", (ctx) => ctx.reply("You pressed B!"));
-// bot.use(mainNenu);
-// Registers the command handlers
+const mainNenu = new Menu("main")
+  .text("Explain medical condition", (ctx) =>
+    ctx.reply("You pressed medical condition")
+  )
+  .row()
+  .text("Explain medication", (ctx) => ctx.reply("You pressed B!"));
+bot.use(mainNenu);
 
-bot.command("start", async (ctx) => {
-  const cmds = new Commands();
+// KEYBOARDS
+const mainKeyboard = new Keyboard()
+  .text("Explain Medical Reports")
+  .row()
+  .text("Explain Medication")
+  .row()
+  .resized();
+const explainMedicalReportKeyboard = new Keyboard()
+  .text("Go back")
+  .row()
+  .resized();
 
-  cmds.command("English", "english");
-  await ctx.setMyCommands(cmds);
+// COMMANDS
+bot.command("start", (ctx) =>
+  ctx.reply("Welcome to Nursify, seek explanations or medication conditions", {
+    reply_markup: mainKeyboard,
+  })
+);
 
-  return ctx.reply(
-    "Welcome to Nursify, seek explanations or medication conditions via the buttons below!"
-  );
+// ON
+bot.on("Explain Medical Reports", (ctx) =>
+  ctx.reply("Upload a picture of the medical report", {
+    reply_markup: explainMedicalReportKeyboard,
+  })
+);
+
+bot.on("Go Back", (ctx) =>
+  ctx.reply("Welcome to Nursify, seek explanations or medication conditions", {
+    reply_markup: mainKeyboard,
+  })
+);
+
+bot.on("message:photo", (ctx) => ctx.reply("You sent a picture"), {
+  reply_markup: mainKeyboard,
 });
 
 export default webhookCallback(bot, "http");
