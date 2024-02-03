4c2fe2f42ec1d8c0d149a0b3fe7c01b989a258d6
keilokimnida <tkl48leon@gmail.com>
2024-02-03 12:13:23 +0800

    [feat] test 2

--- api/bot.js
+++ api/bot.js
@@ -1,4 +1,5 @@
 import { Bot, webhookCallback } from "grammy";
+import { Menu } from "@grammyjs/menu";
 import "dotenv/config";
 
 const token = process.env.BOT_TOKEN;
@@ -6,6 +7,24 @@ if (!token) throw new Error("BOT_TOKEN is unset");
 
 const bot = new Bot(token);
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// MENU
+const mainNenu = new Menu("main")
+  .text("Explain medical condition", (ctx) =>
+    ctx.reply("You pressed medical condition")
+  )
+  .row()
+  .text("Explain medication", (ctx) => ctx.reply("You pressed B!"));
+bot.use(mainNenu);
+
+bot.command(
+  "start",
+  (ctx) =>
+    ctx.reply(
+      "Welcome to Nursify, would you like to explain condition, or explain "
+    ),
+  {
+    reply_markup: mainNenu,
+  }
+);
 
 export default webhookCallback(bot, "http");
