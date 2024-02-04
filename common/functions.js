3eac9fe725652cb327922344ef56410c8cc32329
RandomGeeb <derricklim69lol@gmail.com>
2024-02-04 12:49:20 +0800

    whoops

--- common/functions.js
+++ common/functions.js
@@ -1,3 +1,13 @@
 export function isStringOnlyNumbers(str) {
     return /^\d+$/.test(str);
 }
+
+export function isArrayOnlyNumbers(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (isNaN(Number(arr[i]))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
