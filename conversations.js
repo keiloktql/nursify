792007fa3fa22db03e8a613cfc99747740c8d9a4
RandomGeeb <derricklim69lol@gmail.com>
2024-02-04 13:01:11 +0800

    fix

--- conversations.js
+++ conversations.js
@@ -67,8 +67,8 @@ async function handleReminder(ctx, conversation) {
     if (responseMessage) {
         const timing = [...responseMessage];
         if (timing.length === 4) {
-            const hours = array.slice(0, 2).join("");
-            const minutes = array.slice(-2).join("");
+            const hours = timing.slice(0, 2).join("");
+            const minutes = timing.slice(-2).join("");
             if (hours < 24 && minutes < 60) {
                 response = await enterReminder(hours, minutes);
                 ct.reply(reponse, { reply_markup: goBackKeyboard });
