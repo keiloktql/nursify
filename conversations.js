67f52577726936eccfc691d50dea763f6f82cc51
RandomGeeb <derricklim69lol@gmail.com>
2024-02-04 15:54:04 +0800

    .

--- conversations.js
+++ conversations.js
@@ -98,7 +98,7 @@ async function handleReminder(ctx, conversation) {
 async function fetchReminders(ctx, conversation) {
     const { message, data } = await getReminder();
     if (data) {
-        ctx.reply({ reply_markup: goBackKeyboard });
+        ctx.reply("These are your current reminders:" , { reply_markup: goBackKeyboard });
         data.map((reminder) => {
             const cronArray = reminder.reminder_cron.split("");
             return ctx.reply(
